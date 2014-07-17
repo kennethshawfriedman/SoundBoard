a154c3da81c00471fd204d4adaf093305793c7f3
Kenneth Friedman <kennethshawfriedman@gmail.com>
2014-07-17 02:50:52 -0400

    Project Added

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Kenny Friedman on 7/16/14.
+//  Copyright (c) 2014 Kenneth Friedman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	@IBAction func playSubtle(sender: AnyObject) {
+		
+	}
+
+}
+
